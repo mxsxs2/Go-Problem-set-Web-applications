b36147a493d2faba00749e8213181c8ee80c2836
KRISZTIAN NAGY - Student <G00330024@gmit.ie>
2017-10-17 14:57:40 +0100

    3. Folder fix

--- webapp.go
+++ webapp.go
@@ -6,8 +6,10 @@ import (
 )
 
 func main() {
-	//Add the handler function
-	http.Handle("/", http.FileServer(http.Dir("guess")))
+	//Add the handler function for the landing page
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
 	//Start a webserver which listens at port 8080
 	http.ListenAndServe(":8080", nil)
 }
