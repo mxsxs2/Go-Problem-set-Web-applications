bdd77dee7b224cafe5b830e85a1ed168686f7f0e
KRISZTIAN NAGY - Student <G00330024@gmit.ie>
2017-10-17 14:47:38 +0100

    3. Server a page using Bootstrap

--- webapp.go
+++ webapp.go
@@ -2,23 +2,12 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func main() {
 	//Add the handler function
-	http.HandleFunc("/", requestHandler)
+	http.Handle("/", http.FileServer(http.Dir("guess")))
 	//Start a webserver which listens at port 8080
 	http.ListenAndServe(":8080", nil)
-}
-
-//Handle the request
-func requestHandler(w http.ResponseWriter, r *http.Request) {
-	//Set the html hconent type
-	w.Header().Set("Content-Type", "text/html")
-
-	//Write the response
-	fmt.Fprintln(w, "<h1>Guessing game</h1>")
-
 }
